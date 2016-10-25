04f9e71393f32f75b272598bc0a718f5701aa42f
Bestig <pasha11194@mail.ru>
2016-10-26 02:10:42 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php
+$name = "Шуплецов Павел";
+$age = 21;
+$email = "bestigg@yandex.ru";
+$aboutme = "Студент";
+$town = "Москва";
+ ?>
+ <h1> Страница пользователя  </h1>
+ <h3> Имя пользователя: <?php
+ echo $name;
+ echo PHP_EOL;
+ ?> </h3>
+<h3> Возраст: <?php
+ echo $age;
+ echo PHP_EOL;
+ ?>
+ </h3>
+ <h3> Город: <?php
+  echo $town;
+  echo PHP_EOL;
+  ?>
+  </h3>
+  <h3> Мейл: <?php
+   echo $email;
+   echo PHP_EOL;
+   ?>
+   </h3>
+   <h3> О себе: <?php
+    echo $aboutme;
+    echo PHP_EOL;
+    ?>
+    </h3>
